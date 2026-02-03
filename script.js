8f7579c99fb5d3fe8e267ff61d52633e0257194d
zakiii <mzakim20@gmail.com>
2026-02-03 21:03:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+
+noBtn.addEventListener("mouseover", () => {
+    const x = Math.random() * window.innerWidth * 0.7;
+    const y = Math.random() * window.innerHeight * 0.7;
+
+    noBtn.style.position = "absolute";
+    noBtn.style.left = `${x}px`;
+    noBtn.style.top = `${y}px`;
+});
+
+yesBtn.addEventListener("click", () => {
+    alert("Yay! 🥰 I love you!");
+});
